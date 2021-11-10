8e49835c0347a4b44ccee2abce7552e035466ed3
AbdulMoiz8994 <moiza8994@gmail.com>
2021-11-10 23:23:01 +0500

    add the Project fo initail Gatsby wher we insatll and use plugins contentfull and maerial-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,16 @@ module.exports = {
     title: "My Gatsby Site",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "accessToken",
-        spaceId: "",
-      },
-    },
+    `gatsby-plugin-material-ui`,
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
+   { 
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `ijozn59nr204`,
+      // Learn about environment variables: https://gatsby.dev/env-vars
+      accessToken: 'yv_pCuN12bVmCjJt_xr6d8-48agdbfJhBxjockUfrkM',
+    },
+  },
   ],
 };
